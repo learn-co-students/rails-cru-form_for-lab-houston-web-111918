5f6621b095cd0fd88cf41cde1e12f858a67985ba
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2018-12-21 14:30:44 -0600

    lab solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.create({name: 'Jingle Bells', artist_id: 1, genre_id: 1})
+Song.create({name: 'Rudolph the Red Nosed Reindeer', artist_id: 1, genre_id: 1})
+Song.create({name: 'Thunderstruck', artist_id: 2, genre_id: 2})
+
+Genre.create(name: 'Christmas')
+Genre.create(name: 'Rock')
+
+Artist.create(name: 'Santa')
+Artist.create(name: 'AC/DC')
